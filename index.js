5cfee274a1a60dee9045f7430f509490255e0d3f
philip martinez <martinez.philip00@gmail.com>
2022-06-23 09:43:10 -0500

    commit tree 716b21c6d988b2cb5fb9b348153e4858a6908d1d

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(team) {
+    const winningTeam = team.find(team => team.result === "W");
+    if (winningTeam != undefined) {
+    return (winningTeam.year);
+    }
+};
